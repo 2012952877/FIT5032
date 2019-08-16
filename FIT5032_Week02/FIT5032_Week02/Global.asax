28e318647f3bb96d906dafa5ac313e186ce0e59e
DI CHEN <dche0045@student.monash.edu>
2019-08-16 18:31:44 +1000

    my first upload

---
+++ FIT5032_Week02/FIT5032_Week02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_Week02.MvcApplication" Language="C#" %>
---
+++ FIT5032_Week03/FIT5032_Week03/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_Week03.HelloWorld;
+using FIT5032_Week03.Exercise;
+
+namespace FIT5032_Week03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            Hello hello = new Hello();
+            ViewBag.Message = hello.GetHello();
+
+            ExampleDictionary ed = new ExampleDictionary();
+
+            ed.Example();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
